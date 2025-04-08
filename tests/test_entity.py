90ecfc728c4bde0ac942e82fb77085cb8201caf4
duncathan <dunc@duncathan.com>
2025-04-07 19:33:55 -0600

    test all entity files

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,11 +1,27 @@
+import itertools
+from open_prime_hunters_rando.level_data import ALINOS, ARCTERRA, CELESTIAL_ARCHIVES, CONNECTORS, OUBLIETTE, VESPER_DEFENSE_OUTPOST
 import pytest
 from open_prime_hunters_rando.entities.entity_type import Entity, EntityFile, EntityType
 from construct import Container
 
 
-@pytest.fixture(scope="session")
-def entity_file(rom):
-    return rom.getFileByName("levels/entities/unit2_RM2_Ent.bin")
+all_entity_files = [
+    level.entity_file
+    for level in itertools.chain(
+        CONNECTORS.values(),
+        ALINOS.values(),
+        CELESTIAL_ARCHIVES.values(),
+        VESPER_DEFENSE_OUTPOST.values(),
+        ARCTERRA.values(),
+        OUBLIETTE.values(),
+    )
+    if level.entity_file is not None
+]
+
+
+@pytest.fixture(scope="module", params=all_entity_files)
+def entity_file(rom, request):
+    return rom.getFileByName(f"levels/entities/{request.param}.bin")
 
 
 def test_compare_entity_file(entity_file):
