2bf94e8e11284aba10c4372fc1ba5669ad18f14a
dyceron <dycerontv@gmail.com>
2025-04-22 21:16:37 -0400

    Adjustments to `starting_items` in schema

--- src/open_prime_hunters_rando/arm9.py
+++ src/open_prime_hunters_rando/arm9.py
@@ -8,14 +8,14 @@ def patch_arm9(rom: NintendoDSRom, starting_items: dict) -> None:
     # Validate starting_items string
     _validate_starting_items(starting_items)
 
-    starting_energy = starting_items["starting_ammo"]["energy"].to_bytes(4, "little")
-    starting_ammo = str(hex(starting_items["starting_ammo"]["ammo"] * 10))[2:-1]
-    starting_missiles = starting_items["starting_ammo"]["missiles"].to_bytes()
+    starting_energy = starting_items["energy"].to_bytes(4, "little")
+    starting_ammo = str(hex(starting_items["ammo"] * 10))[2:-1]
+    starting_missiles = starting_items["missiles"].to_bytes()
 
     ARM9_PATCHES = {
         init["starting_ammo"]: bytes.fromhex(starting_ammo),  # Starting UA Ammo
         init["starting_energy"]: bytes.fromhex("00F020E3"),  # NOP (Normally loads value of etank (100))
-        init["starting_weapons"]: int(starting_items["weapons_string"], 2).to_bytes(),  # Starting weapons
+        init["starting_weapons"]: int(starting_items["weapons"], 2).to_bytes(),  # Starting weapons
         init["starting_missiles"]: starting_missiles,  # Starting Missile ammo
         init["unlock_planets"]: bytes.fromhex("FF"),  # Unlock all planets from the start (excluding Oubliette)
         init["starting_energy_ptr"]: starting_energy,  # Starting energy - 1
@@ -40,13 +40,13 @@ def patch_arm9(rom: NintendoDSRom, starting_items: dict) -> None:
 
 def _validate_starting_items(starting_items: dict) -> None:
     # Validate weapons string
-    if len(starting_items["weapons_string"]) != 8:
+    if len(starting_items["weapons"]) != 8:
         raise ValueError(f"Invalid starting items string. Must contain 8 numbers, got {len(starting_items)}!")
 
-    for bit_flag in starting_items["weapons_string"]:
+    for bit_flag in starting_items["weapons"]:
         if bit_flag not in ["0", "1"]:
             raise ValueError(f"Invalid starting items string. String must only contain 0 or 1, got {bit_flag}!")
 
     # Validate starting ammo
-    if starting_items["starting_ammo"]["ammo"] > 400:
+    if starting_items["ammo"] > 400:
         raise ValueError(f"Starting ammo must be 400 or less! Got {starting_items['starting_ammo']['ammo']}")
