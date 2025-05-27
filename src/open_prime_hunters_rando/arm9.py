67405d39e461d03f7898dad0abaf9ed784d0e965
dyceron <dycerontv@gmail.com>
2025-05-26 20:38:59 -0400

    Allow for giving more missiles per tank

--- src/open_prime_hunters_rando/arm9.py
+++ src/open_prime_hunters_rando/arm9.py
@@ -10,7 +10,7 @@ NOP = bytes.fromhex("00F020E3")
 asm_patches = Path(__file__).parent.joinpath("files", "asm_patches")
 
 
-def _create_asm_patch(code: str) -> bytes:
+def create_asm_patch(code: str) -> bytes:
     assembler = keystone.Ks(keystone.KS_ARCH_ARM, keystone.KS_MODE_ARM + keystone.KS_MODE_LITTLE_ENDIAN)
     encoding, count = assembler.asm(code)
     return bytes(encoding)
@@ -37,15 +37,17 @@ def patch_arm9(rom: NintendoDSRom, configuration: dict) -> None:
     reordered_instructions = _read_asm_file("reordered_instructions.s")
 
     ARM9_PATCHES = {
-        arm9["missiles_per_tank"]: (game_patches["missiles_per_tank"] * 10).to_bytes(),  # Missiles per tank
-        arm9["ammo_per_tank"]: _create_asm_patch(f"add r2, r2, #{game_patches['ammo_per_tank'] * 10}"),  # UA per tank
+        arm9["missiles_per_tank"]: create_asm_patch(
+            f"add r2, r2, #{game_patches['missiles_per_tank'] * 10}"
+        ),  # Missiles per tank
+        arm9["ammo_per_tank"]: create_asm_patch(f"add r2, r2, #{game_patches['ammo_per_tank'] * 10}"),  # UA per tank
         arm9["starting_octoliths"]: _bitfield_to_hex(starting_items["octoliths"]),  # Starting Octoliths by changing R0
         arm9["starting_weapons"]: _bitfield_to_hex(starting_items["weapons"]),  # Starting weapons
         arm9["weapon_slots"]: NOP,  # NOP to not delete the weapons when changing Octoliths
         arm9["starting_ammo"]: bytes.fromhex(starting_ammo),  # Starting UA
         arm9["starting_energy"]: NOP,  # NOP (Normally loads value of etank (100))
         arm9["starting_missiles"]: starting_items["missiles"].to_bytes(),  # Starting Missiles
-        arm9["reordered_instructions"]: _create_asm_patch(
+        arm9["reordered_instructions"]: create_asm_patch(
             reordered_instructions
         ),  # Changing R0 affects later instructions, so reorder
         arm9["unlock_planets"]: _unlock_planets(game_patches["unlock_planets"]),  # Unlock planets from the start
