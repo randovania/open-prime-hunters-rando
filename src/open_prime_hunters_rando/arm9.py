e1cb2c9646108d51a128bd073e413d21fc2fe1f9
dyceron <dycerontv@gmail.com>
2025-06-18 22:13:42 -0400

    Use keystone for better handling of asm patching

--- src/open_prime_hunters_rando/arm9.py
+++ src/open_prime_hunters_rando/arm9.py
@@ -1,12 +1,31 @@
+from pathlib import Path
+
+import keystone
 from ndspy.rom import NintendoDSRom
 
 from open_prime_hunters_rando.version_checking import validate_rom
 
+NOP = bytes.fromhex("00F020E3")
+
+asm_patches = Path(__file__).parent.joinpath("files", "asm_patches")
+
+
+def create_asm_patch(code: str) -> bytes:
+    assembler = keystone.Ks(keystone.KS_ARCH_ARM, keystone.KS_MODE_ARM + keystone.KS_MODE_LITTLE_ENDIAN)
+    encoding, count = assembler.asm(code)
+    return bytes(encoding)
+
+
+def _read_asm_file(file: str) -> str:
+    return asm_patches.joinpath(file).read_text()
+
 
 def patch_arm9(rom: NintendoDSRom, configuration: dict) -> None:
-    init = validate_rom(rom)
-    # Validate starting items
+    arm9 = validate_rom(rom)
+
     starting_items = configuration["starting_items"]
+    game_patches = configuration["game_patches"]
+    # Validate starting items
     _validate_starting_items(starting_items)
 
     etanks = starting_items["energy_tanks"]
@@ -15,28 +34,20 @@ def patch_arm9(rom: NintendoDSRom, configuration: dict) -> None:
 
     starting_ammo = str(hex(starting_items["ammo"] * 10))[2:-1]
 
-    reordered_instructions = bytes.fromhex(
-        "2400C4E5"  # strb r0, [r4, 24h]
-        "2600C4E5"  # strb r0, [r4, 26h]
-        "0000A0E3"  # mov r0, #0
-        "0010A0E1"  # mov r1, r0
-        "0020A0E1"  # mov r2, r0
-        "0030A0E1"  # mov r3, r0
-        "BA90C4E1"  # strh sb, [r4, #0xa]
-        "BC80C4E1"  # strh r8, [r4, #0xc]
-        "BE70C4E1"  # strh r7, [r4, #0xe]
-    )
+    reordered_instructions = _read_asm_file("reordered_instructions.s")
 
     ARM9_PATCHES = {
-        init["starting_octoliths"]: _bitfield_to_hex(starting_items["octoliths"]),  # Starting Octoliths by changing R0
-        init["starting_weapons"]: _bitfield_to_hex(starting_items["weapons"]),  # Starting weapons
-        init["weapon_slots"]: bytes.fromhex("00F020E3"),  # NOP to not delete the weapons when changing Octoliths
-        init["starting_ammo"]: bytes.fromhex(starting_ammo),  # Starting UA
-        init["starting_energy"]: bytes.fromhex("00F020E3"),  # NOP (Normally loads value of etank (100))
-        init["starting_missiles"]: starting_items["missiles"].to_bytes(),  # Starting Missiles
-        init["reordered_instructions"]: reordered_instructions,  # Changing R0 affects later instructions, so reorder
-        init["unlock_planets"]: _unlock_planets(configuration["game_patches"]),  # Unlock planets from the start
-        init["starting_energy_ptr"]: starting_energy,  # Starting energy - 1
+        arm9["starting_octoliths"]: _bitfield_to_hex(starting_items["octoliths"]),  # Starting Octoliths by changing R0
+        arm9["starting_weapons"]: _bitfield_to_hex(starting_items["weapons"]),  # Starting weapons
+        arm9["weapon_slots"]: NOP,  # Prevents deleting the weapons when changing Octoliths
+        arm9["starting_ammo"]: bytes.fromhex(starting_ammo),  # Starting UA
+        arm9["starting_energy"]: NOP,  # Normally loads value of etank (100)
+        arm9["starting_missiles"]: starting_items["missiles"].to_bytes(),  # Starting Missiles
+        arm9["reordered_instructions"]: create_asm_patch(
+            reordered_instructions
+        ),  # Changing R0 affects later instructions, so reorder
+        arm9["unlock_planets"]: _unlock_planets(game_patches["unlock_planets"]),  # Unlock planets from the start
+        arm9["starting_energy_ptr"]: starting_energy,  # Starting energy - 1,
     }
 
     # Decompress arm9.bin for editing
@@ -78,8 +89,7 @@ def _bitfield_to_hex(bitfield: str) -> int:
     return int(bitfield, 2).to_bytes()
 
 
-def _unlock_planets(game_patches: dict) -> int:
-    unlock_planets = game_patches["unlock_planets"]
+def _unlock_planets(unlock_planets: dict) -> int:
     planets = [
         unlock_planets["Arcterra"],  # Arcterra 1
         unlock_planets["Arcterra"],  # Arcterra 2
