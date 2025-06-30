34361f8dc1498be27fe11581dcf962d47a4fba9e
dyceron <dycerontv@gmail.com>
2025-06-29 23:40:52 -0400

    Fix palette_id check not working and always change locked doors

--- src/open_prime_hunters_rando/entities/door.py
+++ src/open_prime_hunters_rando/entities/door.py
@@ -4,17 +4,17 @@ from open_prime_hunters_rando.entities.entity_type import EntityFile, PaletteId
 def patch_doors(entity_file: EntityFile, doors: list) -> None:
     for door in doors:
         entity_id = door["entity_id"]
-        palette_id = door["palette_id"]
+        palette_id = PaletteId(door["palette_id"])
 
         entity = entity_file.get_entity(entity_id)
         data = entity.data
 
         # If the new type is the same as the original type, skip changing it
-        if palette_id == data.palette_id:
+        if palette_id == data.palette_id and data.palette_id != PaletteId.LOCKED:
             continue
 
         # Change the weakness
-        data.palette_id = PaletteId(palette_id)
+        data.palette_id = palette_id
 
         # Unlock doors that are changed to Power Beam
         if palette_id == PaletteId.POWER_BEAM:
