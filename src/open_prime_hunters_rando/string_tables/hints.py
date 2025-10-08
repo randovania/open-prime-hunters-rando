1ce87279333681c3450723b85beff7ce8ac354f8
dyceron <dycerontv@gmail.com>
2025-10-07 22:46:05 -0400

    Update schema and patching of hints

--- src/open_prime_hunters_rando/string_tables/hints.py
+++ src/open_prime_hunters_rando/string_tables/hints.py
@@ -4,9 +4,6 @@ from open_prime_hunters_rando.file_manager import FileManager, Language
 def patch_hints(file_manager: FileManager, hints: dict[str, str]) -> None:
     scan_log = file_manager.get_string_table(Language.ENGLISH, "ScanLog")  # TODO: Change other languages
 
-    for hint in hints:
-        string_id = hint["string_id"]
-        text = hint["text"]
-
+    for string_id, text in hints.items():
         string_entry = scan_log.get_string(string_id)
         string_entry.text = text
