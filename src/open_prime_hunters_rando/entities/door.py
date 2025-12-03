2e5a1cc5cb475881fe34bb39f82ba0c794538aed
dyceron <dycerontv@gmail.com>
2025-12-02 20:24:46 -0500

    Add attributes for all entity types

--- src/open_prime_hunters_rando/entities/door.py
+++ src/open_prime_hunters_rando/entities/door.py
@@ -6,24 +6,23 @@ def patch_doors(entity_file: EntityFile, doors: list, room_name: str) -> None:
         entity_id = door["entity_id"]
         palette_id = PaletteId(door["palette_id"])
 
-        entity = entity_file.get_entity(entity_id)
-        data = entity.data
+        entity = entity_file.get_entity(entity_id).door_data()
 
         # Break if trying to change weakness of Morph Ball or Boss doors
-        if data.door_type not in (DoorType.STANDARD, DoorType.THIN):
+        if entity.door_type not in (DoorType.STANDARD, DoorType.THIN):
             raise ValueError(
                 f"Unable to patch entity {entity_id} in {room_name}. Only Standard and Thin door types can be modified."
             )
         # If the new type is the same as the original type, skip changing it
-        if palette_id == data.palette_id:
+        if palette_id == entity.palette_id:
             continue
 
         # Change the weakness
-        data.palette_id = palette_id
+        entity.palette_id = palette_id
 
         # Unlock doors that are changed to Power Beam
         if palette_id == PaletteId.POWER_BEAM:
-            data.locked = False
+            entity.locked = False
         # Activate and lock doors changed to another weapon
         else:
-            data.locked = True
+            entity.locked = True
--- src/open_prime_hunters_rando/entities/force_field.py
+++ src/open_prime_hunters_rando/entities/force_field.py
@@ -5,5 +5,5 @@ def patch_force_fields(entity_file: EntityFile, force_fields: list) -> None:
     for force_field in force_fields:
         entity_id = force_field["entity_id"]
 
-        entity = entity_file.get_entity(entity_id)
-        entity.data.type = PaletteId(force_field["type"])
+        entity = entity_file.get_entity(entity_id).force_field_data()
+        entity.type = PaletteId(force_field["type"])
