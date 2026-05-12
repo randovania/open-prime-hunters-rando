62bd6610b52d211338bc95603c8026d70b82c7ce
dyceron <dycerontv@gmail.com>
2026-05-11 22:13:59 -0400

    Add create() to ForceField and fix Door create()

--- src/open_prime_hunters_rando/parsing/formats/entities/entity_types/force_field.py
+++ src/open_prime_hunters_rando/parsing/formats/entities/entity_types/force_field.py
@@ -1,9 +1,12 @@
+import typing
+
 from construct import Construct, Flag, Struct
 
 from open_prime_hunters_rando.parsing.common_types import FixedPoint, PaletteIdConstruct
+from open_prime_hunters_rando.parsing.common_types.vectors import Vec3
 from open_prime_hunters_rando.parsing.formats.entities.base_entity import Entity
 from open_prime_hunters_rando.parsing.formats.entities.entity_classes import field
-from open_prime_hunters_rando.parsing.formats.entities.enum import PaletteId
+from open_prime_hunters_rando.parsing.formats.entities.enum import EntityType, PaletteId
 
 ForceFieldEntityData = Struct(
     "force_field_type" / PaletteIdConstruct,
@@ -24,3 +27,36 @@ class ForceField(Entity):
     height = field(float)
 
     active = field(bool)
+
+    @classmethod
+    def cls_entity_type(cls) -> EntityType:
+        return EntityType.FORCE_FIELD
+
+    @classmethod
+    def create(
+        cls,
+        node_name: str = "",
+        layer_state: typing.Sequence[bool] = (False,) * 16,
+        entity_id: int = -1,
+        position: Vec3 | tuple[float, float, float] = (0.0, 0.0, 0.0),
+        up_vector: Vec3 | tuple[float, float, float] = (0.0, 1.0, 0.0),
+        facing_vector: Vec3 | tuple[float, float, float] = (0.0, 0.0, 1.0),
+        force_field_type: PaletteId = PaletteId.LOCKED,
+        width: float = 0.0,
+        height: float = 0.0,
+        active: bool = False,
+    ) -> typing.Self:
+        obj = super().create(
+            node_name,
+            layer_state,
+            entity_id,
+            position,
+            up_vector,
+            facing_vector,
+        )
+        obj.force_field_type = force_field_type
+        obj.width = width
+        obj.height = height
+        obj.active = active
+
+        return obj
