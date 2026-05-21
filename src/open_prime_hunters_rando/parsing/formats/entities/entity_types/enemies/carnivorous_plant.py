a52d2840ca41c5e32ff2fe089a737854c445127d
dyceron <dycerontv@gmail.com>
2026-05-20 22:01:29 -0400

    Add remaining EnemyTypes
    Also fixes Platform

--- src/open_prime_hunters_rando/parsing/formats/entities/entity_types/enemies/carnivorous_plant.py
+++ src/open_prime_hunters_rando/parsing/formats/entities/entity_types/enemies/carnivorous_plant.py
@@ -1,8 +1,10 @@
+import typing
+
 from construct import Construct, Int16ul, Int32ul, Struct
 
-from open_prime_hunters_rando.parsing.common_types.volume import BaseVolumeType, CollisionVolume
+from open_prime_hunters_rando.parsing.common_types.volume import BaseVolumeType, CollisionVolume, SphereVolumeType
 from open_prime_hunters_rando.parsing.formats.entities.entity_classes import field
-from open_prime_hunters_rando.parsing.formats.entities.entity_types.enemies.enemy_base import EnemyFields
+from open_prime_hunters_rando.parsing.formats.entities.entity_types.enemies.enemy_base import EnemyFields, EnemyType
 
 CarnivorousPlantEntityData = Struct(
     "enemy_health" / Int16ul,
@@ -22,3 +24,27 @@ class CarnivorousPlant(EnemyFields, default_field_location="raw"):
     enemy_subtype = field(int)
 
     volume = field(BaseVolumeType)
+
+    @classmethod
+    def cls_enemy_type(cls) -> EnemyType:
+        return EnemyType.CARNIVOROUS_PLANT
+
+    @classmethod
+    def create(
+        cls,
+        enemy_health: int = 4,
+        enemy_damage: int = 1,
+        enemy_subtype: int = 37,
+        volume: BaseVolumeType | None = None,
+    ) -> typing.Self:
+        if volume is None:
+            volume = SphereVolumeType.create()
+
+        carnivorous_plant = super().create()
+
+        carnivorous_plant.enemy_health = enemy_health
+        carnivorous_plant.enemy_damage = enemy_damage
+        carnivorous_plant.enemy_subtype = enemy_subtype
+        carnivorous_plant.volume = volume
+
+        return carnivorous_plant
--- src/open_prime_hunters_rando/parsing/formats/entities/entity_types/enemies/shriek_bat.py
+++ src/open_prime_hunters_rando/parsing/formats/entities/entity_types/enemies/shriek_bat.py
@@ -1,9 +1,16 @@
+import typing
+
 from construct import Construct, Struct
 
 from open_prime_hunters_rando.parsing.common_types.vectors import Vec3, Vector3Fx
-from open_prime_hunters_rando.parsing.common_types.volume import BaseVolumeType, CollisionVolume
+from open_prime_hunters_rando.parsing.common_types.volume import (
+    BaseVolumeType,
+    BoxVolumeType,
+    CollisionVolume,
+    SphereVolumeType,
+)
 from open_prime_hunters_rando.parsing.formats.entities.entity_classes import field
-from open_prime_hunters_rando.parsing.formats.entities.entity_types.enemies.enemy_base import EnemyFields
+from open_prime_hunters_rando.parsing.formats.entities.entity_types.enemies.enemy_base import EnemyFields, EnemyType
 
 ShriekBatEntityData = Struct(
     "volume1" / CollisionVolume,
@@ -24,3 +31,31 @@ class ShriekBat(EnemyFields, default_field_location="raw"):
 
     volume2 = field(BaseVolumeType)
     volume3 = field(BaseVolumeType)
+
+    @classmethod
+    def cls_enemy_type(cls) -> EnemyType:
+        return EnemyType.SHRIEKBAT
+
+    @classmethod
+    def create(
+        cls,
+        volume1: BaseVolumeType | None = None,
+        path_vector: Vec3 | tuple[float, float, float] = (0.0, 0.0, 0.0),
+        volume2: BaseVolumeType | None = None,
+        volume3: BaseVolumeType | None = None,
+    ) -> typing.Self:
+        if volume1 is None:
+            volume1 = SphereVolumeType.create()
+        if volume2 is None:
+            volume2 = BoxVolumeType.create()
+        if volume3 is None:
+            volume3 = BoxVolumeType.create()
+
+        shriekbat = super().create()
+
+        shriekbat.volume1 = volume1
+        shriekbat.path_vector = Vec3(*path_vector)
+        shriekbat.volume2 = volume2
+        shriekbat.volume3 = volume3
+
+        return shriekbat
