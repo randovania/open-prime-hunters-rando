6b4c27931f8dccfe24363c41e5e58cff7e8a1c9f
dyceron <dycerontv@gmail.com>
2026-06-06 21:49:33 -0400

    Update comments and formatting, remove unlock cutscenes in Data Shrine 01/03

--- src/open_prime_hunters_rando/patching/entities/shield_key_patches.py
+++ src/open_prime_hunters_rando/patching/entities/shield_key_patches.py
@@ -25,7 +25,7 @@ def patch_shield_key_rooms(file_manager: FileManager) -> None:
 def _high_ground(file_manager: FileManager) -> None:
     high_ground = file_manager.get_entity_file("Alinos", "High Ground")
 
-    # Only move the plaform when the Artifact is collected
+    # Only move the plaform after the artifact is collected
     artifact = high_ground.get_entity(24, Artifact)
     artifact.message1_target = 94
     artifact.message1 = Message.TRIGGER
@@ -47,10 +47,9 @@ def _high_ground(file_manager: FileManager) -> None:
         child_id=56,
         child_message=Message.UNLOCK_CONNECTORS,
     )
-
     high_ground.append_entity(unlocking_trigger)
 
-    # Add a new trigger that activates the Artifact and the door unlocking trigger
+    # Add a new trigger that activates the artifact and the door unlocking trigger
     post_spire_trigger = TriggerVolume.create(
         node_name="rmMain",
         layer_state=spire2.layer_state,
@@ -61,10 +60,9 @@ def _high_ground(file_manager: FileManager) -> None:
         child_id=unlocking_trigger.entity_id,
         child_message=Message.ACTIVATE,
     )
-
     high_ground.append_entity(post_spire_trigger)
 
-    # Replace the message thats activates the Artifact with moving the item spawner
+    # Replace the message thats activates the artifact with moving the item spawner
     # and activate the new post-Spire trigger
     spire2.message1_target = 81
     spire2.message1 = Message.MOVE_ITEM_SPAWNER
@@ -75,7 +73,7 @@ def _high_ground(file_manager: FileManager) -> None:
 def _elder_passage(file_manager: FileManager) -> None:
     elder_passage = file_manager.get_entity_file("Alinos", "Elder Passage")
 
-    # Move the message that unlocks the doors from the Artifact to the Camera Sequence
+    # Move the message that unlocks the doors from the artifact to the camera sequence
     artifact = elder_passage.get_entity(4, Artifact)
     artifact.message1_target = -1
     artifact.message1 = Message.NONE
@@ -88,7 +86,7 @@ def _elder_passage(file_manager: FileManager) -> None:
 def _piston_cave(file_manager: FileManager) -> None:
     piston_cave = file_manager.get_entity_file("Alinos", "Piston Cave")
 
-    # Prevent the doors from locking
+    # Remove the messages that lock the doors
     trigger = piston_cave.get_entity(82, TriggerVolume)
     trigger.parent_id = -1
     trigger.parent_message = Message.NONE
@@ -106,26 +104,54 @@ def _piston_cave(file_manager: FileManager) -> None:
 def _data_shrine_01(file_manager: FileManager) -> None:
     data_shrine_01 = file_manager.get_entity_file("Celestial Archives", "Data Shrine 01")
 
-    # Move the message that unlocks the doors from the Artifact to the Camera Sequence
+    # Remove the message that unlocks the doors
     artifact = data_shrine_01.get_entity(2, Artifact)
     artifact.message1_target = -1
     artifact.message1 = Message.NONE
 
+    # Add a new trigger that unlocks the doors after the fight
+    door_unlocking_trigger = TriggerVolume.create(
+        node_name=artifact.node_name,
+        layer_state=artifact.layer_state,
+        subtype=TriggerVolumeType.AUTOMATIC,
+        active=False,
+        parent_id=8,
+        parent_message=Message.UNLOCK,
+        child_id=9,
+        child_message=Message.UNLOCK,
+    )
+    data_shrine_01.append_entity(door_unlocking_trigger)
+
+    # Activate the new trigger after the shield key cutscene
     camera_sequence = data_shrine_01.get_entity(10, CameraSequence)
-    camera_sequence.end_message_target_id = 21
+    camera_sequence.end_message_target_id = door_unlocking_trigger.entity_id
     camera_sequence.end_message = Message.ACTIVATE
 
 
 def _data_shrine_03(file_manager: FileManager) -> None:
     data_shrine_03 = file_manager.get_entity_file("Celestial Archives", "Data Shrine 03")
 
-    # Move the message that unlocks the doors from the Artifact to the Camera Sequence
+    # Remove the message that unlocks the doors
     artifact = data_shrine_03.get_entity(2, Artifact)
     artifact.message1_target = -1
     artifact.message1 = Message.NONE
 
+    # Add a new trigger that unlocks the doors after the fight
+    door_unlocking_trigger = TriggerVolume.create(
+        node_name=artifact.node_name,
+        layer_state=artifact.layer_state,
+        subtype=TriggerVolumeType.AUTOMATIC,
+        active=False,
+        parent_id=4,
+        parent_message=Message.UNLOCK,
+        child_id=7,
+        child_message=Message.UNLOCK,
+    )
+    data_shrine_03.append_entity(door_unlocking_trigger)
+
+    # Activate the new trigger after the shield key cutscene
     camera_sequence = data_shrine_03.get_entity(42, CameraSequence)
-    camera_sequence.end_message_target_id = 22
+    camera_sequence.end_message_target_id = door_unlocking_trigger.entity_id
     camera_sequence.end_message = Message.ACTIVATE
 
 
@@ -151,19 +177,19 @@ def _synergy_core(file_manager: FileManager) -> None:
 def _stasis_bunker(file_manager: FileManager) -> None:
     stasis_bunker = file_manager.get_entity_file("Vesper Defense Outpost", "Stasis Bunker")
 
-    # Lock the doors when the first Artifact is collected instead of when entering the room
+    # Lock the doors when the first artifact is collected instead of when entering the room
     first_artifact = stasis_bunker.get_entity(5, Artifact)
     first_artifact.message2_target = 1
     first_artifact.message2 = Message.LOCK
     first_artifact.message3_target = 2
     first_artifact.message3 = Message.LOCK
 
-    # Remove the message that unlocks the doors from the second Artifact
+    # Remove the message that unlocks the doors from the second artifact
     second_artifact = stasis_bunker.get_entity(4, Artifact)
     second_artifact.message1_target = -1
     second_artifact.message1 = Message.NONE
 
-    # Change the trigger that locked the doors to unlock the doors after the second Shield Key spawns
+    # Change the trigger that locked the doors to unlock the doors after the second shield key spawns
     door_locking_trigger = stasis_bunker.get_entity(34, TriggerVolume)
     door_locking_trigger.subtype = TriggerVolumeType.AUTOMATIC
     door_locking_trigger.active = False
@@ -179,20 +205,19 @@ def _stasis_bunker(file_manager: FileManager) -> None:
 def _sic_transit(file_manager: FileManager) -> None:
     sic_transit = file_manager.get_entity_file("Arcterra", "Sic Transit")
 
-    # Remove the locking messages from the Artifact
+    # Remove the locking messages from the artifact
     artifact = sic_transit.get_entity(35, Artifact)
     artifact.message1_target = -1
     artifact.message1 = Message.NONE
     artifact.message2_target = -1
     artifact.message2 = Message.NONE
 
-    # Unlock the doors after the Shield Key spawns
+    # Unlock the doors after the shield key spawns
     door_locking_trigger = sic_transit.get_entity(20, TriggerVolume)
 
     unlocking_trigger = copy.deepcopy(door_locking_trigger)
     unlocking_trigger.parent_message = Message.UNLOCK
     unlocking_trigger.child_message = Message.UNLOCK
-
     sic_transit.append_entity(unlocking_trigger)
 
     camera_sequence = sic_transit.get_entity(23, CameraSequence)
