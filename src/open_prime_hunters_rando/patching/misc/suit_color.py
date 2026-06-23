eb584cf126b52e7570c0165cfc101aa2b0c5fea9
dyceron <dycerontv@gmail.com>
2026-06-22 22:07:02 -0400

    Use names directly instead of numbers

--- src/open_prime_hunters_rando/patching/misc/suit_color.py
+++ src/open_prime_hunters_rando/patching/misc/suit_color.py
@@ -6,25 +6,25 @@ from open_prime_hunters_rando.logger import LOG
 
 
 class SuitColor(enum.Enum):
-    DEFAULT = 0
-    PURPLE = 1
-    BLUE = 2
-    BLACK = 3
-    ORANGE = 4
-    GREEN = 5
+    VARIA = "varia"
+    PURPLE = "purple"
+    BLUE = "blue"
+    BLACK = "black"
+    ORANGE = "orange"
+    GREEN = "green"
 
 
-def patch_suit_color(rom: NintendoDSRom, suit_color: int) -> None:
-    # Only modify the color if it not default
+def patch_suit_color(rom: NintendoDSRom, suit_color: str) -> None:
+    # Only modify the color if it not varia
     new_color = SuitColor(suit_color)
-    if new_color == SuitColor.DEFAULT:
+    if new_color == SuitColor.VARIA:
         return
 
     LOG.info("Changing suit color")
 
     # Mapping of suit color to file names
     suit_color_mapping = {
-        SuitColor.DEFAULT: ["01", "01"],
+        SuitColor.VARIA: ["01", "01"],
         SuitColor.PURPLE: ["02", "02"],
         SuitColor.BLUE: ["03", "03"],
         SuitColor.BLACK: ["04", "04"],
@@ -32,17 +32,17 @@ def patch_suit_color(rom: NintendoDSRom, suit_color: int) -> None:
         SuitColor.GREEN: ["team02", "Team02"],
     }
 
-    # Default files to replace
-    default_samus_tex_path = "models/Samus_pal_01_Tex.bin"
-    default_gun_model_path = "models/SamusGun_img_01_Model.bin"
-    default_gun_tex_path = "models/SamusGun_img_01_Tex.bin"
+    # File paths for textures and models of varia Samus and gun
+    varia_samus_tex_path = "models/Samus_pal_01_Tex.bin"
+    varia_gun_model_path = "models/SamusGun_img_01_Model.bin"
+    varia_gun_tex_path = "models/SamusGun_img_01_Tex.bin"
 
     # Get the model files associated with the new color
     new_samus_tex = rom.getFileByName(f"models/Samus_pal_{suit_color_mapping[new_color][0]}_Tex.bin")
     new_gun_model = rom.getFileByName(f"models/SamusGun_img_{suit_color_mapping[new_color][1]}_Model.bin")
     new_gun_tex = rom.getFileByName(f"models/SamusGun_img_{suit_color_mapping[new_color][1]}_Tex.bin")
 
-    # Replace the bytes of the default models with the bytes of the new color
-    rom.setFileByName(default_samus_tex_path, new_samus_tex)
-    rom.setFileByName(default_gun_tex_path, new_gun_tex)
-    rom.setFileByName(default_gun_model_path, new_gun_model)
+    # Replace the bytes of the varia models with the bytes of the new color
+    rom.setFileByName(varia_samus_tex_path, new_samus_tex)
+    rom.setFileByName(varia_gun_tex_path, new_gun_tex)
+    rom.setFileByName(varia_gun_model_path, new_gun_model)
